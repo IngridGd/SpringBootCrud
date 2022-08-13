f2fb8b93c9b54c53f99c9a24e8b73b220077d363
ingridGd <ingridgama@live.com>
2022-08-13 20:36:26 -0300

    Atualizando o crud da api

---
+++ src/main/java/br/com/alura/forum/Controller/Form/AtualizacaoTopicoForm.java
@@ -0,0 +1,2 @@
+package br.com.alura.forum.Controller.Form;public class AtualizacaoTopicoForm {
+}
---
+++ src/main/java/br/com/alura/forum/Controller/dto/DetalhesDoTopicoDto.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum.Controller.dto;
+
+import java.time.LocalDateTime;
+
+public class DetalhesDoTopicoDto {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+    private String NomeAutor;
+}
